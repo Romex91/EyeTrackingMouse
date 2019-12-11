5724b28934a813de88c1133a3f04280b84554407
Roman Kuksin <rkuksin.cpp@gmail.com>
2019-12-11 16:37:01 +0400

    add assembly description

--- eye_tracking_mouse/Properties/AssemblyInfo.cs
+++ eye_tracking_mouse/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EyeTrackingMouse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EyeTrackingMouse. A computer mouse replacement with combination of Tobii Eye tracker 4c and hotkeys.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EyeTrackingMouse")]
