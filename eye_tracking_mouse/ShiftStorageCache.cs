740bd99f692d6ac947157262f7dbbf9ebbd68514
Roman Kuksin <rkuksin.cpp@gmail.com>
2020-04-09 12:50:58 +0700

    Style

--- eye_tracking_mouse/ShiftStorageCache.cs
+++ eye_tracking_mouse/ShiftStorageCache.cs
@@ -12,6 +12,27 @@ namespace eye_tracking_mouse
     public class ShiftStorageCache
     {
         static readonly int AlignedCoordinatesCount = Math.Max(8, System.Numerics.Vector<double>.Count);
+        // All coordinates involved in the performance bottleneck calculations are stored here.
+        // Rationale is to decrease number of cache misses.
+        // The coordinates count is alligned to 8-dimensions. 
+        //    E.G. if you use 6-dimensional space last two dimensions will always be zero.
+        // First 8-doubles are allocated for cursor position.
+        // Next 8 * mode.max_zones_count are allocated for adjusted coordinates of saved points
+        // After cursor position changes there is calculation of distance from cursor position to each saved point.
+        // Next 8 * mode.max_zones_count are allocated for the results of subtract operation.
+        // Next mode.max_zones_count are allocated for the results of distance calculations.
+        private readonly double[] cached_data;
+
+        private struct StartingIndex
+        {
+            public int cached_coordinates;
+            public int subtract_results;
+            public int distances;
+        }
+        private readonly StartingIndex starting_index;
+        private Options.CalibrationMode mode;
+        private int number_of_shift_positions = 0;
+        private double[] coordinate_scales;
 
         public ShiftStorageCache(Options.CalibrationMode mode)
         {
@@ -22,36 +43,20 @@ namespace eye_tracking_mouse
             this.mode.max_zones_count = aligned_max_zones_count;
 
             var scales_in_percents = mode.additional_dimensions_configuration.CoordinatesScalesInPercents;
-            CoordinateScales = scales_in_percents.Select(x => x/100.0).ToArray();
+            coordinate_scales = scales_in_percents.Select(x => x/100.0).ToArray();
 
-            CoordinatesCache = new double[ 
+            cached_data = new double[ 
                 AlignedCoordinatesCount +                                   // cursor position
                 AlignedCoordinatesCount * mode.max_zones_count +            // cached coordinates
                 AlignedCoordinatesCount * mode.max_zones_count +            // subtruction results
                 mode.max_zones_count];                                      // distances
 
-            SavedCoordinatesStartingIndex = AlignedCoordinatesCount;
-            SubtractResultsStartingIndex = AlignedCoordinatesCount * mode.max_zones_count + SavedCoordinatesStartingIndex;
-            DistancesStartingIndex = AlignedCoordinatesCount * mode.max_zones_count + SubtractResultsStartingIndex;
-        }
-
-        public double[] CoordinateScales {
-            private set;
-            get;
-        }
-
-        // All coordinates involved in the performance bottleneck calculations are stored here.
-        // Rationale is to decrease number of cache misses.
-        // The coordinates count is alligned to 8-dimensions. 
-        //    E.G. if you use 6-dimensional space last two dimensions will always be zero.
-        // First 8-doubles are allocated for cursor position.
-        // Next 8 * mode.max_zones_count are allocated for adjusted coordinates of saved points
-        // After cursor position changes there is calculation of distance from cursor position to each saved point.
-        // Next 8 * mode.max_zones_count are allocated for the results of subtract operation.
-        // Next mode.max_zones_count are allocated for the results of distance calculations.
-        public double[] CoordinatesCache {
-            private set;
-            get;
+            starting_index = new StartingIndex
+            {
+                cached_coordinates = AlignedCoordinatesCount,
+                subtract_results = AlignedCoordinatesCount * mode.max_zones_count + AlignedCoordinatesCount,
+                distances = 2 * AlignedCoordinatesCount * mode.max_zones_count + AlignedCoordinatesCount
+            };
         }
 
         public class PointInfo
@@ -68,21 +73,6 @@ namespace eye_tracking_mouse
             get;
         }
 
-        public int SavedCoordinatesStartingIndex
-        {
-            private set;
-            get;
-        }
-        public int SubtractResultsStartingIndex
-        {
-            private set;
-            get;
-        }
-        public int DistancesStartingIndex
-        {
-            private set;
-            get;
-        }
 
         public int AllocateIndex()
         {
@@ -94,10 +84,10 @@ namespace eye_tracking_mouse
             number_of_shift_positions--;
             for (int i = index; i < number_of_shift_positions; ++i)
             {
-                int saved_coordinates_index = SavedCoordinatesStartingIndex + i * AlignedCoordinatesCount;
+                int saved_coordinates_index = starting_index.cached_coordinates + i * AlignedCoordinatesCount;
                 for (int j = 0; j < AlignedCoordinatesCount; j++)
-                    CoordinatesCache[saved_coordinates_index + j] =
-                        CoordinatesCache[saved_coordinates_index + j + AlignedCoordinatesCount];
+                    cached_data[saved_coordinates_index + j] =
+                        cached_data[saved_coordinates_index + j + AlignedCoordinatesCount];
             }
         }
 
@@ -106,27 +96,42 @@ namespace eye_tracking_mouse
             number_of_shift_positions = 0;
         }
 
-        private int GetCoordinatesShift(int index)
-        {
-            return index == -1 ? 0 : SavedCoordinatesStartingIndex + index * AlignedCoordinatesCount;
-        }
-
         public void SaveToCache(double[] coordinates, int cache_index)
         {
             Debug.Assert(coordinates.Length <= AlignedCoordinatesCount);
-            Debug.Assert(coordinates.Length == CoordinateScales.Length);
+            Debug.Assert(coordinates.Length == coordinate_scales.Length);
 
             int i = 0;
+            int coordinates_shift =  starting_index.cached_coordinates + cache_index * AlignedCoordinatesCount;
             for (; i < coordinates.Length; ++i)
             {
-                CoordinatesCache[GetCoordinatesShift(cache_index) + i] =
-                    CoordinateScales[i] * coordinates[i];
+                cached_data[coordinates_shift + i] =
+                    coordinate_scales[i] * coordinates[i];
             }
             for (; i < AlignedCoordinatesCount; ++i)
             {
-                CoordinatesCache[GetCoordinatesShift(cache_index) + i] = 0;
+                cached_data[coordinates_shift + i] = 0;
+            }
+        }
+
+        private double[] GetSubtractionResult(int cache_index)
+        {
+            Debug.Assert(cache_index >= 0);
+
+            var retval = new double[coordinate_scales.Length];
+            int subtract_results_index = starting_index.subtract_results + cache_index * AlignedCoordinatesCount;
+            for (int i = 0; i < retval.Length; i++)
+            {
+                retval[i] = cached_data[subtract_results_index + i];
             }
 
+            return retval;
+        }
+
+        public double GetDistanceFromCursor(int cache_index)
+        {
+            Debug.Assert(cache_index >= 0);
+            return cached_data[starting_index.distances + cache_index];
         }
 
         // Performs cacluclation of distance from cursor to each saved point.
@@ -147,35 +152,35 @@ namespace eye_tracking_mouse
             for (int i = 0; i < vectors_per_point; ++i)
             {
                 cursor_position[i] = new System.Numerics.Vector<double>(
-                    CoordinatesCache,
+                    cached_data,
                     i * vector_size);
             }
 
             System.Numerics.Vector<double> zero = System.Numerics.Vector<double>.Zero;
             for (int i = 0; i < number_of_shift_positions; i += vector_size)
             {
-                zero.CopyTo(CoordinatesCache, i + DistancesStartingIndex);
+                zero.CopyTo(cached_data, i + starting_index.distances);
             }
 
             for (int i = 0; i < number_of_shift_positions * vectors_per_point; ++i)
             {
-                int saved_coordinates_index = i * vector_size + SavedCoordinatesStartingIndex;
-                int subtract_results_index = i * vector_size + SubtractResultsStartingIndex;
+                int saved_coordinates_index = i * vector_size + starting_index.cached_coordinates;
+                int subtract_results_index = i * vector_size + starting_index.subtract_results;
 
                 var saved_coordinates = new System.Numerics.Vector<double>(
-                    CoordinatesCache, 
+                    cached_data, 
                     saved_coordinates_index);
                 var subtract_result = (saved_coordinates - cursor_position[i % vectors_per_point]);
-                subtract_result.CopyTo(CoordinatesCache, subtract_results_index);
+                subtract_result.CopyTo(cached_data, subtract_results_index);
 
-                CoordinatesCache[DistancesStartingIndex + i / vectors_per_point] +=
+                cached_data[starting_index.distances + i / vectors_per_point] +=
                     System.Numerics.Vector.Dot(subtract_result, subtract_result);
             }
 
             for (int i = 0; i < number_of_shift_positions; i += vector_size)
             {
-                var dot_products_vec = new System.Numerics.Vector<double>(CoordinatesCache, i + DistancesStartingIndex);
-                System.Numerics.Vector.SquareRoot(dot_products_vec).CopyTo(CoordinatesCache, i + DistancesStartingIndex);
+                var dot_products_vec = new System.Numerics.Vector<double>(cached_data, i + starting_index.distances);
+                System.Numerics.Vector.SquareRoot(dot_products_vec).CopyTo(cached_data, i + starting_index.distances);
             }
         }
 
@@ -185,26 +190,26 @@ namespace eye_tracking_mouse
             // Subtract 
             for (int i = 0; i < number_of_shift_positions * AlignedCoordinatesCount; i += AlignedCoordinatesCount)
             {
-                int saved_coordinates_index = i + SavedCoordinatesStartingIndex;
-                int subtract_results_index = i + SubtractResultsStartingIndex;
+                int saved_coordinates_index = i + starting_index.cached_coordinates;
+                int subtract_results_index = i + starting_index.subtract_results;
                 for (int j = 0; j < AlignedCoordinatesCount; ++j)
                 {
-                    CoordinatesCache[subtract_results_index + j] = CoordinatesCache[saved_coordinates_index + j] - CoordinatesCache[j];
+                    cached_data[subtract_results_index + j] = cached_data[saved_coordinates_index + j] - cached_data[j];
                 }
             }
 
             // Length of subtract results
             for (int i = 0; i < number_of_shift_positions; ++i)
             {
-                int subtract_results_index = i * AlignedCoordinatesCount + SubtractResultsStartingIndex;
+                int subtract_results_index = i * AlignedCoordinatesCount + starting_index.subtract_results;
                 double dot_product = 0;
                 for (int j = 0; j < AlignedCoordinatesCount; ++j)
                 {
-                    double k = CoordinatesCache[subtract_results_index + j];
+                    double k = cached_data[subtract_results_index + j];
                     dot_product += k * k;
                 }
-                int distance_index = i + DistancesStartingIndex;
-                CoordinatesCache[distance_index] = Math.Sqrt(dot_product);
+                int distance_index = i + starting_index.distances;
+                cached_data[distance_index] = Math.Sqrt(dot_product);
             }
         }
 
@@ -219,12 +224,12 @@ namespace eye_tracking_mouse
             {
                 indexes_closest_to_cursor[i] = int.MaxValue;
             }
-            for (int i = DistancesStartingIndex; i < DistancesStartingIndex + number_of_shift_positions; ++i)
+            for (int i = starting_index.distances; i < starting_index.distances + number_of_shift_positions; ++i)
             {
                 int j = 0;
                 for (; j < indexes_count; j++)
                 {
-                    if (CoordinatesCache[i] < CoordinatesCache[indexes_closest_to_cursor[j]])
+                    if (cached_data[i] < cached_data[indexes_closest_to_cursor[j]])
                     {
                         for (int k = indexes_closest_to_cursor.Length - 1; k > j; k--)
                         {
@@ -255,34 +260,12 @@ namespace eye_tracking_mouse
             {
                 ClosestPoints.Add(new PointInfo
                 {
-                    distance = CoordinatesCache[index] > 0.0001 ? CoordinatesCache[index] : 0.0001,
-                    index = index - DistancesStartingIndex,
-                    vector_from_correction_to_cursor = GetSubtractionResult(index - DistancesStartingIndex),
+                    distance = cached_data[index] > 0.0001 ? cached_data[index] : 0.0001,
+                    index = index - starting_index.distances,
+                    vector_from_correction_to_cursor = GetSubtractionResult(index - starting_index.distances),
                     weight = 1
                 });
             }
         }
-        public double[] GetSubtractionResult(int cache_index)
-        {
-                Debug.Assert(cache_index >= 0);
-
-                var retval = new double[CoordinateScales.Length];
-                int subtract_results_index = SubtractResultsStartingIndex + cache_index * AlignedCoordinatesCount;
-                for (int i = 0; i < retval.Length; i++)
-                {
-                    retval[i] = CoordinatesCache[subtract_results_index + i];
-                }
-
-                return retval;
-        }
-
-        public double GetDistanceFromCursor(int cache_index)
-        {
-                Debug.Assert(cache_index >= 0);
-                return CoordinatesCache[DistancesStartingIndex + cache_index];
-        }
-
-        private Options.CalibrationMode mode;
-        private int number_of_shift_positions = 0;
     }
 }
