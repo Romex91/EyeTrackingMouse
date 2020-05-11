ada3b9b29da291b6105b71c954c6fd46d48e416f
Roman Kuksin <rkuksin.cpp@gmail.com>
2020-05-11 16:09:50 +0700

    try disabling coordinates during mode generation

--- BlindConfigurationTester/CalibrationModeIterator.cs
+++ BlindConfigurationTester/CalibrationModeIterator.cs
@@ -27,19 +27,19 @@ namespace BlindConfigurationTester
             var fields = new OptionsField[]
             {
                 OptionsField.BuildHardcoded(field_name : "zone_size", new List<int>{ 10, 15, 25, 50, 75, 100, 150, 200, 250, 350, 500, 800 }),
-                OptionsField.BuildExponential(field_name : "max_zones_count", 8, 2048, 2),
+                OptionsField.BuildExponential(field_name : "max_zones_count", 8, 2048, 2, false),
                 OptionsField.BuildHardcoded(field_name : "considered_zones_count", new List<int>{ 3,  6, 10, 20 }),
                 OptionsField.BuildLinear(field_name : "size_of_opaque_sector_in_percents", max : 70, min : 30, step: 10),
                 OptionsField.BuildLinear(field_name : "size_of_transparent_sector_in_percents", max : 60, min : 0, step: 10),
                 OptionsField.BuildHardcoded(field_name : "correction_fade_out_distance", new List<int>{ 50, 75, 100, 150, 200, 250, 350, 500, 800 }),
-                OptionsField.BuildExponential(field_name : "coordinate 2", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 3", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 4", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 5", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 6", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 7", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 8", 50, 30000, 1.6f),
-                OptionsField.BuildExponential(field_name : "coordinate 9", 50, 30000, 1.6f),
+                OptionsField.BuildExponential(field_name : "coordinate 2", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 3", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 4", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 5", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 6", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 7", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 8", 50, 30000, 1.6f, true),
+                OptionsField.BuildExponential(field_name : "coordinate 9", 50, 30000, 1.6f, true),
                 //OptionsField.BuildHardcoded(field_name : "coordinate 2", new List<int> {50, 100, 250, 400, 600, 800, 1000, 1300, 1700, 2500, 5000, 7500, 10000, 12000 }),
                 //OptionsField.BuildHardcoded(field_name : "coordinate 3", new List<int> {50, 100, 250, 400, 600, 800, 1000, 1300, 1700, 2500, 5000, 7500, 10000, 12000 }),
                 //OptionsField.BuildHardcoded(field_name : "coordinate 4", new List<int> {50, 100, 250, 400, 600, 800, 1000, 1300, 1700, 2500, 5000, 7500, 10000, 12000 }),
@@ -59,46 +59,63 @@ namespace BlindConfigurationTester
             }
         }
 
-        public interface IntRange
-        {
-            List<int> GetRange();
-        }
-
         public class OptionsField
         {
             public string field_name;
-            private IntRange range;
-
             public int[] Range
             {
-                get
-                {
-                    return range.GetRange().ToArray();
-                }
+                private set;
+                get;
             }
 
             public int Min
             {
                 get
                 {
-                    return range.GetRange().First();
+                    return Range.First();
                 }
             }
 
             public static OptionsField BuildLinear(string field_name, int min, int max, int step)
             {
+                List<int> range = new List<int>();
+                int i = min;
+                while (true)
+                {
+                    if (i <= max)
+                        range.Add(i);
+                    else
+                        break;
+                    i = (int)(i + step);
+                    
+                }
+
                 return new OptionsField
                 {
                     field_name = field_name,
-                    range = new IteartionRange { min_value = min, max_value = max, step = step, exponential = false }
+                    Range = range.ToArray()
                 };
             }
-            public static OptionsField BuildExponential(string field_name, int min, int max, float step)
+            public static OptionsField BuildExponential(string field_name, int min, int max, float step, bool include_zero)
             {
+                List<int> range = new List<int>();
+                if (include_zero)
+                    range.Add(0);
+                int i = min;
+                while (true)
+                {
+                    if (i <= max)
+                        range.Add(i);
+                    else
+                        break;
+
+                    i = (int)(i * step);
+                }
+
                 return new OptionsField
                 {
                     field_name = field_name,
-                    range = new IteartionRange { min_value = min, max_value = max, step = step, exponential = true }
+                    Range = range.ToArray()
                 };
             }
             public static OptionsField BuildHardcoded(string field_name, List<int> range)
@@ -106,7 +123,7 @@ namespace BlindConfigurationTester
                 return new OptionsField
                 {
                     field_name = field_name,
-                    range = new HardcodedIntRange { range = range }
+                    Range = range.ToArray()
                 };
             }
 
@@ -160,65 +177,21 @@ namespace BlindConfigurationTester
                     return false;
                 }
 
-                var range = this.range.GetRange();
                 int i = 0;
-                for (; i < range.Count; i++)
+                for (; i < Range.Length; i++)
                 {
-                    if (range[i] >= value)
+                    if (Range[i] >= value)
                     {
                         break;
                     }
                 }
 
                 i += steps_number;
-                if (i < 0 || i >= range.Count)
+                if (i < 0 || i >= Range.Length)
                     return false;
 
-                SetFieldValue(calibration_mode, range[i]);
+                SetFieldValue(calibration_mode, Range[i]);
                 return true;
-            }
-        }
-
-        private struct IteartionRange : IntRange
-        {
-            public int min_value;
-            public int max_value;
-            public float step;
-            public bool exponential;
-
-            public List<int> GetRange()
-            {
-                List<int> range = new List<int>();
-
-
-                int i = min_value;
-                while (true)
-                {
-                    if (i <= max_value)
-                        range.Add(i);
-                    else
-                        break;
-
-                    if (exponential)
-                    {
-                        i = (int)(i * step);
-                    }
-                    else
-                    {
-                        i = (int)(i + step);
-                    }
-                }
-
-                return range;
-            }
-        }
-
-        private struct HardcodedIntRange : IntRange
-        {
-            public List<int> range;
-            public List<int> GetRange()
-            {
-                return range;
             }
         }
 
@@ -239,7 +212,8 @@ namespace BlindConfigurationTester
                     }
                     else
                     {
-                        Fields[field_number].SetFieldValue(mode, Fields[field_number].Range.First());
+                        var range = Fields[field_number].Range;
+                        Fields[field_number].SetFieldValue(mode, (range[0] == 0 && range.Length > 1) ? range[1] : range[0]);
                     }
                 }
 
