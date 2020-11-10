e1d4cef32aab72dcd29196c71c6b0b16b5b02bb2
Roman Kuksin <rkuksin.cpp@gmail.com>
2020-11-10 13:19:27 +0700

    update copyright year

--- eye_tracking_mouse/Properties/AssemblyInfo.cs
+++ eye_tracking_mouse/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EyeTrackingMouse")]
-[assembly: AssemblyCopyright("Copyright © 2019 Roman Kuksin")]
+[assembly: AssemblyCopyright("Copyright © 2020 Roman Kuksin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
