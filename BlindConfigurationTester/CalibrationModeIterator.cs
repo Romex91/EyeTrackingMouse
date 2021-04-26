85e423b7599163136005ed2401a7b8caeeed3c02
Roman Kuksin <rkuksin.cpp@gmail.com>
2021-04-26 14:32:44 +0700

    Remove old experimental testing code (Study, Configuration utils)
    The old approach of gathering DataSets proved to be wrong.

--- BlindConfigurationTester/CalibrationModeIterator.cs
+++ BlindConfigurationTester/CalibrationModeIterator.cs
@@ -8,6 +8,22 @@ using System.Windows;
 
 namespace BlindConfigurationTester
 {
+    public static partial class Extensions
+    {
+        public static void Shuffle<T>(this IList<T> list, Random rng)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
     public class CalibrationModeIterator
     {
         public eye_tracking_mouse.Options.CalibrationMode CalibrationMode
--- BlindConfigurationTester/Utils.cs
+++ BlindConfigurationTester/Utils.cs
@@ -11,25 +11,6 @@ namespace BlindConfigurationTester
 {
     class  Utils
     {
-        public static bool IsApplicationOpen()
-        {
-            var processes = Process.GetProcesses();
-            foreach (var process in processes)
-            {
-                if (process.ProcessName == eye_tracking_mouse.Helpers.application_name)
-                    return true;
-            }
-            return false;
-        }
-
-        public static bool TryCloseApplication()
-        {
-            if (!IsApplicationOpen())
-                return true;
-
-            return (new CloseAppDialog()).ShowDialog()== true;
-        }
-
         public static string[] GetConfigurationsList()
         {
             try
@@ -46,13 +27,6 @@ namespace BlindConfigurationTester
             }
         }
 
-        public static void CopyDir(string source, string destination)
-        {
-            // C# is fucking great!
-            new Microsoft.VisualBasic.Devices.Computer().
-                FileSystem.CopyDirectory(source, destination);
-        }
-
         public static string DataFolder
         {
             get { return Path.Combine( Environment.GetEnvironmentVariable("LocalAppData"), "BlindConfigurationTester"); }
@@ -63,24 +37,6 @@ namespace BlindConfigurationTester
             if (configuration == null)
                 return eye_tracking_mouse.Helpers.UserDataFolder;
             return Path.Combine(DataFolder, "Configurations", configuration);
-        }
-
-        public static void SaveToUserData(string configuration)
-        {
-            if (configuration == null)
-                return;
-            string configuration_path = GetConfigurationDir(configuration);
-            if (!Directory.Exists(configuration_path))
-            {
-                MessageBox.Show("Configuration " + configuration + " doesn't exist!");
-                return;
-            }
-
-            if (!TryCloseApplication())
-                return;
-
-            Directory.Delete(eye_tracking_mouse.Helpers.UserDataFolder, true);
-            CopyDir(configuration_path, eye_tracking_mouse.Helpers.UserDataFolder);
         }
 
         public static string GenerateNewConfigurationName(
@@ -107,95 +63,12 @@ namespace BlindConfigurationTester
             return tag+"_" +
                 (generated_configs_max_index + 1);
         }
-        public static void LoadFromUserData(string configuration)
-        {
-            string configuration_path = GetConfigurationDir(configuration);
-            if (configuration_path == eye_tracking_mouse.Helpers.UserDataFolder)
-                return;
-            if (!TryCloseApplication())
-                return;
-            Directory.Delete(configuration_path, true);
-            CopyDir(eye_tracking_mouse.Helpers.UserDataFolder, configuration_path);
-        }
-
-        private static void RunAppFromUserData(bool save_progress, Action before_start, Action after_start)
-        {
-            var user_data_backup = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-            if (!save_progress)
-            {
-                if (!TryCloseApplication())
-                    return;
-                CopyDir(eye_tracking_mouse.Helpers.UserDataFolder, user_data_backup);
-            }
-
-
-            before_start?.Invoke();
-
-            if (!IsApplicationOpen())
-                Process.Start(eye_tracking_mouse.Helpers.ExePath);
-
-            after_start?.Invoke();
-
-            if (!save_progress)
-            {
-                while (!TryCloseApplication());
-                Directory.Delete(eye_tracking_mouse.Helpers.UserDataFolder, true);
-                Directory.Move(user_data_backup, eye_tracking_mouse.Helpers.UserDataFolder);
-            }
-        }
-
-        public static void RunApp(string configuration, bool save_progress, Action before_start, Action after_start)
-        {
-            if (configuration == null)
-            {
-                RunAppFromUserData(save_progress, before_start, after_start);
-                return;
-            }
-
-            string configuration_path = GetConfigurationDir(configuration);
-            if (!Directory.Exists(configuration_path))
-            {
-                MessageBox.Show("Configuration " + configuration + " doesn't exist!");
-                return;
-            }
-
-            if (!TryCloseApplication())
-                return;
-
-            var user_data_backup = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-            Directory.Move(eye_tracking_mouse.Helpers.UserDataFolder, user_data_backup);
-            if (save_progress)
-                Directory.Move(configuration_path, eye_tracking_mouse.Helpers.UserDataFolder);
-            else
-                CopyDir(configuration_path, eye_tracking_mouse.Helpers.UserDataFolder);
-
-            before_start?.Invoke();
-            Process.Start(eye_tracking_mouse.Helpers.ExePath);
-            after_start?.Invoke();
-
-            while (!TryCloseApplication());
-            if (save_progress)
-                Directory.Move(eye_tracking_mouse.Helpers.UserDataFolder, configuration_path);
-            else
-                Directory.Delete(eye_tracking_mouse.Helpers.UserDataFolder, true);
-            Directory.Move(user_data_backup, eye_tracking_mouse.Helpers.UserDataFolder);
-        }
 
         public static void CreateConfiguration(string configuration)
         {
             string configuration_path = GetConfigurationDir(configuration);
             if (!Directory.Exists(configuration_path))
                 Directory.CreateDirectory(configuration_path);
-        }
-
-        public static void RemoveConfiguration(string configuration)
-        {
-            string configuration_path = GetConfigurationDir(configuration);
-            if (configuration == null && !TryCloseApplication())
-                return;
-
-            if (Directory.Exists(configuration_path))
-                Directory.Delete(configuration_path, true);
         }
     }
 }
